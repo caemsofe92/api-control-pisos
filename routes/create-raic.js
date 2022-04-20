b8b941691df2b63d5016908b00c8a9f3c5c8701f
carlos.soto <carlos.soto@srfconsultores.com>
2022-04-20 15:44:04 -0500

    Commit Base

---
+++ routes/create-raic.js
@@ -0,0 +1,274 @@
+let express = require("express");
+let router = express.Router();
+const axios = require("axios");
+const client = require("../bin/redis-client");
+const moment = require("moment");
+
+router.post("/", async (req, res) => {
+  try {
+    const tenantUrl = req.query.tenantUrl || (req.body && req.body.tenantUrl);
+    const clientId = req.query.clientId || (req.body && req.body.clientId);
+    const clientSecret =
+      req.query.clientSecret || (req.body && req.body.clientSecret);
+    const tenant = req.query.tenant || (req.body && req.body.tenant);
+    const environment =
+      req.query.environment || (req.body && req.body.environment);
+    const unsafeCondition =
+      req.query.unsafeCondition || (req.body && req.body.unsafeCondition);
+    const improvementOpportunity =
+      req.query.improvementOpportunity ||
+      (req.body && req.body.improvementOpportunity);
+
+    const eventDetails =
+      req.query.eventDetails || (req.body && req.body.eventDetails);
+    const eventCauses =
+      req.query.eventCauses || (req.body && req.body.eventCauses);
+    const potentialEventDamage =
+      req.query.potentialEventDamage ||
+      (req.body && req.body.potentialEventDamage);
+    const evidences = req.query.evidences || (req.body && req.body.evidences);
+
+    if (!tenantUrl || tenantUrl.length === 0)
+      throw new Error("tenantUrl is Mandatory");
+
+    if (!clientId || clientId.length === 0)
+      throw new Error("clientId is Mandatory");
+
+    if (!clientSecret || clientSecret.length === 0)
+      throw new Error("clientSecret is Mandatory");
+
+    if (!tenant || tenant.length === 0) throw new Error("tenant is Mandatory");
+
+    if (!environment || environment.length === 0)
+      throw new Error("environment is Mandatory");
+
+    if (!unsafeCondition || unsafeCondition.length === 0)
+      throw new Error("unsafeCondition is Mandatory");
+
+    if (!client.isOpen) client.connect();
+
+    let token = await client.get(environment);
+
+    if (!token) {
+      const tokenResponse = await axios
+        .post(
+          `https://login.microsoftonline.com/${tenantUrl}/oauth2/token`,
+          `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}&resource=${tenant}/`,
+          { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+        )
+        .catch(function (error) {
+          if (
+            error.response &&
+            error.response.data &&
+            error.response.data.error &&
+            error.response.data.error.innererror &&
+            error.response.data.error.innererror.message
+          ) {
+            throw new Error(error.response.data.error.innererror.message);
+          } else if (error.request) {
+            throw new Error(error.request);
+          } else {
+            throw new Error("Error", error.message);
+          }
+        });
+      token = tokenResponse.data.access_token;
+      await client.set(environment, tokenResponse.data.access_token, {
+        EX: 3599,
+      });
+    }
+
+    let _unsafeCondition = await axios
+      .post(
+        `${tenant}/data/SRF_HSEUnsafeConditionsReport?$format=application/json;odata.metadata=none`,
+        {
+          ...unsafeCondition,
+          Responsible: unsafeCondition.Responsible.toString(),
+          UtcDrawingDate: moment(unsafeCondition.UtcDrawingDate).add(5, 'hours')
+        },
+        { headers: { Authorization: "Bearer " + token } }
+      )
+      .catch(function (error) {
+        if (
+          error.response &&
+          error.response.data &&
+          error.response.data.error &&
+          error.response.data.error.innererror &&
+          error.response.data.error.innererror.message
+        ) {
+          throw new Error(error.response.data.error.innererror.message);
+        } else if (error.request) {
+          throw new Error(error.request);
+        } else {
+          throw new Error("Error", error.message);
+        }
+      });
+
+    _unsafeCondition = _unsafeCondition.data;
+
+    let _improvementOpportunity;
+
+    if (improvementOpportunity) {
+      _improvementOpportunity = await axios
+        .post(
+          `${tenant}/api/services/SRF_HSEDocuRefServicesGroup/SRF_HSEDocuRefServices/createOpportunities`,
+          {
+            _description: improvementOpportunity,
+            _dataAreaId: _unsafeCondition.dataAreaId,
+            _idOrigin: _unsafeCondition.SRF_HSEIdUnsafeCondition,
+            _detectionDate: _unsafeCondition.UtcDrawingDate,
+            _refRecId: _unsafeCondition.RecId1,
+            _state: 0,
+            _hcmEmploymentType: 0,
+            _origin: 8,
+            _tableID: 20371,
+          },
+          {
+            headers: { Authorization: "Bearer " + token },
+          }
+        )
+        .catch(function (error) {
+          if (
+            error.response &&
+            error.response.data &&
+            error.response.data.error &&
+            error.response.data.error.innererror &&
+            error.response.data.error.innererror.message
+          ) {
+            throw new Error(error.response.data.error.innererror.message);
+          } else if (error.request) {
+            throw new Error(error.request);
+          } else {
+            throw new Error("Error", error.message);
+          }
+        });
+      _improvementOpportunity = _improvementOpportunity.data;
+    }
+
+    let _eventDetails;
+
+    if (eventDetails) {
+      _eventDetails = await axios
+        .post(
+          `${tenant}/data/SRF_HSEEventDetails?$format=application/json;odata.metadata=none`,
+          {
+            dataAreaId: _unsafeCondition.dataAreaId,
+            SRF_HSEIdUnsafeCondition: _unsafeCondition.SRF_HSEIdUnsafeCondition,
+            ...eventDetails,
+            EventDate2: moment(eventDetails.EventDate2).add(5, 'hours')
+          },
+          {
+            headers: { Authorization: "Bearer " + token },
+          }
+        )
+        .catch(function (error) {
+          if (
+            error.response &&
+            error.response.data &&
+            error.response.data.error &&
+            error.response.data.error.innererror &&
+            error.response.data.error.innererror.message
+          ) {
+            throw new Error(error.response.data.error.innererror.message);
+          } else if (error.request) {
+            throw new Error(error.request);
+          } else {
+            throw new Error("Error", error.message);
+          }
+        });
+      _eventDetails = _eventDetails.data;
+    }
+
+    let _eventCauses = [];
+
+    if (eventCauses) {
+      for (let i = 0; i < eventCauses.length; i++) {
+        const cause = eventCauses[i];
+        const causeResponse = await axios
+          .post(
+            `${tenant}/data/SRF_HSEEventCauses?$format=application/json;odata.metadata=none`,
+            {
+              dataAreaId: _eventDetails.dataAreaId,
+              SRF_HSEIdUnsafeCondition: _eventDetails.SRF_HSEIdUnsafeCondition,
+              RefRecid: _eventDetails.RecId1,
+              ...cause,
+              Description:undefined
+            },
+            {
+              headers: { Authorization: "Bearer " + token },
+            }
+          )
+          .catch(function (error) {
+            if (
+              error.response &&
+              error.response.data &&
+              error.response.data.error &&
+              error.response.data.error.innererror &&
+              error.response.data.error.innererror.message
+            ) {
+              throw new Error(error.response.data.error.innererror.message);
+            } else if (error.request) {
+              throw new Error(error.request);
+            } else {
+              throw new Error("Error", error.message);
+            }
+          });
+        _eventCauses.push(causeResponse.data);
+      }
+    }
+
+    let _potentialEventDamage = [];
+
+    if (potentialEventDamage) {
+      for (let i = 0; i < potentialEventDamage.length; i++) {
+        const damage = potentialEventDamage[i];
+        const damageResponse = await axios
+          .post(
+            `${tenant}/data/SRF_HSEPotentialEventDamage?$format=application/json;odata.metadata=none`,
+            {
+              dataAreaId: _eventDetails.dataAreaId,
+              SRF_HSEIdUnsafeCondition: _eventDetails.SRF_HSEIdUnsafeCondition,
+              RefRecid: _eventDetails.RecId1,
+              ...damage,
+            },
+            {
+              headers: { Authorization: "Bearer " + token },
+            }
+          )
+          .catch(function (error) {
+            if (
+              error.response &&
+              error.response.data &&
+              error.response.data.error &&
+              error.response.data.error.innererror &&
+              error.response.data.error.innererror.message
+            ) {
+              throw new Error(error.response.data.error.innererror.message);
+            } else if (error.request) {
+              throw new Error(error.request);
+            } else {
+              throw new Error("Error", error.message);
+            }
+          });
+        _potentialEventDamage.push(damageResponse.data);
+      }
+    }
+
+    return res.json({
+      result: true,
+      message: "OK",
+      _unsafeCondition,
+      _improvementOpportunity,
+      _eventDetails,
+      _eventCauses,
+      _potentialEventDamage,
+      _evidences: [],
+    });
+  } catch (error) {
+    return res.status(500).json({
+      result: false,
+      message: error.toString(),
+    });
+  }
+});
+
+module.exports = router;
