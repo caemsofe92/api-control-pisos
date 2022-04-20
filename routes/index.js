57f0830766df737eab71983cfe22b810298e7ad0
carlos.soto <carlos.soto@srfconsultores.com>
2022-04-20 16:18:21 -0500

    Commit Prueba

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.send("HSEQ API");
+  res.send("Control de Pisos API");
 });
 
 module.exports = router;
