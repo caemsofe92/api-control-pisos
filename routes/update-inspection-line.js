f4c49bd1051303c31d947ebbbeb548febde34968
carlos.soto <carlos.soto@srfconsultores.com>
2022-05-27 12:27:39 -0500

    Commit

---
+++ routes/update-inspection-line.js
@@ -0,0 +1,113 @@
+let express = require("express");
+let router = express.Router();
+const axios = require("axios");
+const client = require("../bin/redis-client");
+
+router.post("/", async (req, res) => {
+  try {
+    const tenantUrl = req.query.tenantUrl || (req.body && req.body.tenantUrl);
+    const clientId = req.query.clientId || (req.body && req.body.clientId);
+    const clientSecret =
+      req.query.clientSecret || (req.body && req.body.clientSecret);
+    const tenant = req.query.tenant || (req.body && req.body.tenant);
+    const environment =
+      req.query.environment || (req.body && req.body.environment);
+    const inspectionLines =
+      req.query.inspectionLines || (req.body && req.body.inspectionLines);
+
+    if (!tenantUrl || tenantUrl.length === 0)
+      throw new Error("tenantUrl is Mandatory");
+
+    if (!clientId || clientId.length === 0)
+      throw new Error("clientId is Mandatory");
+
+    if (!clientSecret || clientSecret.length === 0)
+      throw new Error("clientSecret is Mandatory");
+
+    if (!tenant || tenant.length === 0) throw new Error("tenant is Mandatory");
+
+    if (!environment || environment.length === 0)
+      throw new Error("environment is Mandatory");
+
+    if (!client.isOpen) client.connect();
+
+    let token = await client.get(environment);
+
+    if (!token) {
+      const tokenResponse = await axios
+        .post(
+          `https://login.microsoftonline.com/${tenantUrl}/oauth2/token`,
+          `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}&resource=${tenant}/`,
+          { headers: { "Content-Type": "application/x-www-form-urlencoded" } }
+        )
+        .catch(function (error) {
+          if (
+            error.response &&
+            error.response.data &&
+            error.response.data.error &&
+            error.response.data.error.innererror &&
+            error.response.data.error.innererror.message
+          ) {
+            throw new Error(error.response.data.error.innererror.message);
+          } else if (error.request) {
+            throw new Error(error.request);
+          } else {
+            throw new Error("Error", error.message);
+          }
+        });
+      token = tokenResponse.data.access_token;
+      await client.set(environment, tokenResponse.data.access_token, {
+        EX: 3599,
+      });
+    }
+
+    let _inspectionLines = [];
+
+    if (inspectionLines && inspectionLines.length > 0) {
+      for (let i = 0; i < inspectionLines.length; i++) {
+        const inspection = inspectionLines[i];
+        const inspectionResponse = await axios
+          .patch(
+            `${tenant}/data/SRF_AMInspectionLines(dataAreaId='${inspection.dataAreaId}',RecId1=${inspection.RecId1})?cross-company=true`,
+            inspection,
+            {
+              headers: { Authorization: "Bearer " + token },
+            }
+          )
+          .catch(function (error) {
+            if (
+              error.response &&
+              error.response.data &&
+              error.response.data.error &&
+              error.response.data.error.innererror &&
+              error.response.data.error.innererror.message
+            ) {
+              throw new Error(error.response.data.error.innererror.message);
+            } else if (error.request) {
+              throw new Error(error.request);
+            } else {
+              throw new Error("Error", error.message);
+            }
+          });
+        _inspectionLines.push(
+          inspectionResponse && inspectionResponse.data === ""
+            ? "Modified"
+            : "Unchanged"
+        );
+      }
+    }
+
+    return res.json({
+      result: true,
+      message: "OK",
+      _inspectionLines,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      result: false,
+      message: error.toString(),
+    });
+  }
+});
+
+module.exports = router;
