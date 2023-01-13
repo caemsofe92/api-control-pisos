a53f61b6e432c08b65553adb061d4d37ff87f520
GonzaloLoaiza <galoaiza88@misena.edu.co>
2023-01-12 19:51:17 -0500

    Commit

--- routes/get-conditions.js
+++ routes/get-conditions.js
@@ -200,7 +200,7 @@ router.post("/", async (req, res) => {
         isTest && numberOfElements ? "&$top=" + numberOfElements : ""
       }&cross-company=true${
         userCompany ? `&$filter=dataAreaId eq '${userCompany}'` : ""
-      }&$select=GroupId,ProjGroupId,Description&$orderby=asc`,
+      }&$select=GroupId,ProjGroupId,Description`,
       { headers: { Authorization: "Bearer " + token } }
     );
 
@@ -209,7 +209,7 @@ router.post("/", async (req, res) => {
         isTest && numberOfElements ? "&$top=" + numberOfElements : ""
       }&cross-company=true${
         userCompany ? `&$filter=dataAreaId eq '${userCompany}'` : ""
-      }&$select=TypeId,Description&$orderby=asc`,
+      }&$select=TypeId,Description`,
       { headers: { Authorization: "Bearer " + token } }
     );
 
