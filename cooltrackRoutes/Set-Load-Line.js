76823854a3361b59aeb6effd94a3255cc69ff915
Carlos Soto <carlosemsofe@gmail.com>
2023-06-22 10:38:31 -0500

    Commit

--- cooltrackRoutes/Set-Load-Line.js
+++ cooltrackRoutes/Set-Load-Line.js
@@ -103,8 +103,8 @@ router.post("/", async (req, res) => {
   const tenantUrl = "navitrans.com.co";
   const clientId = "97a8cc5c-65a7-40ac-b1b8-4c9f50e2bc3b";
   const clientSecret = "31n8Q~rvYbbRtLqcEhmOob5zhHQPCjQO4611jcZ7";
-  const tenant = "https://nav-devpa63a793dcc9b33505devaos.axcloud.dynamics.com";
-  const environment = "Desarrollo";
+  const tenant = "https://uat3-navitrans.sandbox.operations.dynamics.com";
+  const environment = "UAT3";
 
   if (!client.isOpen) client.connect();
 
