9244c025810b4c0ed5f67d308be59c2a649831b1
GonzaloLoaiza <galoaiza88@misena.edu.co>
2023-07-07 08:37:38 -0500

    commit cambio emdpoint

--- cooltrackRoutes/set-invoice-header.js
+++ cooltrackRoutes/set-invoice-header.js
@@ -91,8 +91,8 @@ router.post("/", async (req, res) => {
   const tenantUrl = "navitrans.com.co";
   const clientId = "97a8cc5c-65a7-40ac-b1b8-4c9f50e2bc3b";
   const clientSecret = "31n8Q~rvYbbRtLqcEhmOob5zhHQPCjQO4611jcZ7";
-  const tenant = "https://uat3-navitrans.sandbox.operations.dynamics.com";
-  const environment = "UAT3";
+  const tenant = "https://uat-navitrans.sandbox.operations.dynamics.com";
+  const environment = "UAT";
 
   if (!client.isOpen) client.connect();
 
