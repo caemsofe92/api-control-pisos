57de164e2ad599d963ae7c8de3c2815c89e6d7ce
Carlos Soto <carlosemsofe@gmail.com>
2023-08-01 13:49:21 -0500

    Commit

--- cooltrackRoutes/search-invoice-pakingslip.js
+++ cooltrackRoutes/search-invoice-pakingslip.js
@@ -10,7 +10,7 @@ router.post("/", async (req, res) => {
     const tenantUrl = "navitrans.com.co";
     const clientId = "97a8cc5c-65a7-40ac-b1b8-4c9f50e2bc3b";
     const clientSecret ="31n8Q~rvYbbRtLqcEhmOob5zhHQPCjQO4611jcZ7";
-    const tenant = "https://uat3-navitrans.sandbox.operations.dynamics.com";
+    const tenant = "https://uat4-navitrans.sandbox.operations.dynamics.com";
     const environment ="UAT3";
     const search =
       req.query.search || (req.body && req.body.search);
